31b20f00067d6583fa27c30be23fac5f21657afa
Levi Miller <paullevimiller@gmail.com>
2019-03-21 08:14:38 -0700

    Implemented Ingredient and Price adder and remover.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,24 +4,102 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+// all caps for global constants
+const INGREDIENT_PRICES = {
+    salad: 0.49,
+    cheese: 0.49,
+    meat: 1.49,
+    bacon: 0.99
+};
+
 class BurgerBuilder extends Component {
     // constructor(props) {
     //     super(props);
     //     this.state = {...}
     // }
     state = {
-        ingredients : {
+        ingredients: {
             salad: 0,
             bacon: 0,
             cheese: 0,
-            meat: 0        
-        }
+            meat: 0
+        },
+        totalPrice: 3.99,
+        purchasable: false
     }
+
+    // updatePurchaseState (ingredients) {
+    //     const sum = Object.keys( ingredients )
+    //         .map( igKey => {
+    //             return ingredients[igKey];
+    //         } )
+    //         .reduce( ( sum, el ) => {
+    //             return sum + el;
+    //         }, 0 );
+    //     this.setState( { purchasable: sum > 0 } );
+    // }
+
+    updatePurchaseState (ingredients) {
+        const sum = Object.keys(ingredients)
+            .map(igKey => {
+                return ingredients[igKey];
+            })
+            .reduce((sum, el) => {
+                return sum + el;
+            }, 0);
+        this.setState({purchasable: sum > 0});
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        // State should be updated in an immutable way.
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+        this.updatePurchaseState(updatedIngredients);
+    }
+
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        if (oldCount <= 0) {
+            return;
+        }
+        const updatedCount = oldCount - 1;
+        // State should be updated in an immutable way.
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+        this.updatePurchaseState(updatedIngredients);
+    }
+
     render () {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0
+        }
+        // {sald: true, meat: false, ...}
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
-                <BuildControls />
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler} 
+                    disabled={disabledInfo}
+                    purchasable={this.state.purchasable} 
+                    price={this.state.totalPrice} />
             </Aux>
         );
     }
